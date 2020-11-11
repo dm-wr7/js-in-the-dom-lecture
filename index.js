9c3d3f61b5e2f5cbad220dba415f5ec683e76726
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-11-11 11:34:55 -0500

    Ready for lecture

---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log to confirm our files have been linked
+
+// Use document.querySelector to select the DOM node (HTML element) with a class of 'title'
+
+//Use the .innerText property to change the title of the website
+
+//Use the .style property to change how the title looks
+
+//You can also dig into the .style property to set specific styles.  Change the fontSize of our title
+//These are seen as inline styles so they override anything in your linked .css file
+
+//You can access .classList to receive a list of all the classes assigned to a specific element
+//.classList has built in .add, .remove, and .toggle methods to modify this list
+//Add the class 'dark-mode' to the header text
+
+//Use document.getElementById to grab the form with an id of 'color-form'
+
+//Write a function to change the background color of the div with a class of 'content-hold' to whatever is typed in to the color input.
+
+//Use .addEventListener to attach the color change function to the submit event of the color-form
+
+//Demonstrate event bubbling by attaching an event listener to the 'todo-hold' div.
+
+//Complete the functionality of our todo list
+//Include a warning if the user attempts to submit an empty todo
